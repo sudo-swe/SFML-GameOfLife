0749378d435f85977991b3d7edf27189f4f8f8fd
Jeffery Frederic <168811279+sudfish@users.noreply.github.com>
2024-07-31 16:05:47 -0700

    definitions and perfected compilation script

---
+++ include/DEFINITIONS.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800 
+
+#define GAME_TITLE "Conway's Game Of Life"
+
+#define PATH_SPLASH_BACKGROUND "../assets/res/SplashBackground.png"
