f89887a64a064ecd8a55ebaa8c474202b5f5fee9
sudo-swe <168811279+sudo-swe@users.noreply.github.com>
2024-08-11 20:10:07 -0700

    added rgb colors

--- include/DEFINITIONS.hpp
+++ include/DEFINITIONS.hpp
@@ -30,5 +30,22 @@
 #define CELL_HEIGHT (SCREEN_HEIGHT-(2*BOARD_MARGIN))/BOARD_COLUMNS
 #define CELL_OUTLINE_THICKNESS 1.0f
 #define CELL_ALIVE_FILL_COLOR sf::Color::Green
+#define CELL_ALIVE_RANDOM_COLOR_1 sf::Color(250, 221, 35)
+#define CELL_ALIVE_RANDOM_COLOR_2 sf::Color(35, 250, 232)
+#define CELL_ALIVE_RANDOM_COLOR_3 sf::Color(35, 250, 39)
+#define CELL_ALIVE_RANDOM_COLOR_4 sf::Color(35, 35, 250)
+#define CELL_ALIVE_RANDOM_COLOR_5 sf::Color(211, 35, 250)
+#define CELL_ALIVE_RANDOM_COLOR_6 sf::Color(250, 153, 35)
 #define CELL_DEAD_FILL_COLOR sf::Color::Black
+#define CELL_DEAD_TRAIL_1 sf::Color(190, 0, 0)
+#define CELL_DEAD_TRAIL_2 sf::Color(150, 0, 0)
+#define CELL_DEAD_TRAIL_3 sf::Color(120, 0, 0)
+#define CELL_DEAD_TRAIL_4 sf::Color(100, 0, 0)
+#define CELL_DEAD_TRAIL_5 sf::Color(80, 0, 0)
+#define CELL_DEAD_TRAIL_6 sf::Color(70, 0, 0)
+#define CELL_DEAD_TRAIL_7 sf::Color(60, 0, 0)
+#define CELL_DEAD_TRAIL_8 sf::Color(50, 0, 0)
+#define CELL_DEAD_TRAIL_9 sf::Color(30, 0, 0)
+#define CELL_DEAD_TRAIL_10 sf::Color(15, 0, 0)
+#define CELL_DEAD_TRAIL_11 sf::Color(0, 0, 0)
 #define CELL_OUTLINE_COLOR sf::Color::White
